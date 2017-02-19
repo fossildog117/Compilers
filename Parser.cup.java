d3a520e6caa8ed9fd4603e5bc9acd22edb025d9c
fossildog117 <quantumbiology@hotmail.co.uk>
2017-02-19 01:34:47 +0000

    Update Parser.cup.java

--- Parser.cup.java
+++ Parser.cup.java
@@ -3,29 +3,29 @@ import java.util.ArrayList;
 
 parser code {:
 
-    public boolean syntaxErrors = false;
+public boolean syntaxErrors = false;
 
-    private Lexer lexer;
+private Lexer lexer;
 
-    public Parser(Lexer input) {
-        super(input);
-        lexer = input;
+public Parser(Lexer input) {
+super(input);
+lexer = input;
 
-    }
+}
 
-    public void syntax_errors(Symbol symbol) {
-
-        report_error("Syntax error at line " + (symbol.left+1) + " , column " + symbol.right);
-        syntaxErrors = true;
-
-    }
+public void syntax_errors(Symbol symbol) {
+report_error("Syntax error at line " + (symbol.left+1) + " , column " + symbol.right);
+syntaxErrors = true;
+}
 
 
 :};
 
 scan with {:
-    return lexer.next_token();
+return lexer.next_token();
 :};
+
+// ------- Grammar Values ------- START -------
 
 // Reserved words
 terminal MAIN, BOOLEAN, ICHARACTER, DICTIONARY, XRATIONAL, XINTEGER, IFLOAT, TOP, SEQ, IN, TRUE, FALSE, TYPEDEF, FUNCDEF, VOID, ALIAS, IF, ELIF, ELSE, FORALL, WHILE, READ, PRINT, RETURN, OD, DO, FI, LEN, THEN
@@ -38,6 +38,25 @@ terminal RATIONAL, FLOAT, INTEGER, NUMBER, CHARACTER, IDENTIFIER
 
 // Error flag
 terminal ERRORFLAG
+
+// ------- Grammar Values ------- END -------
+
+// Precedence
+// Values with lowest line number have lowest precedence
+precedence left AND, OR, NOT;
+precedence left LESSEQUAL, MOREEQUAL, ISEQUAL, NOTEQUAL, MORETHAN, LESSTHAN;
+precedence left DCOLON;
+precedence left PLUS, MINUS;
+precedence left MULT, DIV;
+precedence left POWER;
+precedence left LPAREN, RPAREN;
+
+// Non terminals
+nonterminals main, function_body
+
+// Grammar
+main ::= MAIN LCURLY function_body RCURLY SEMICOL;
+
 
 
 
