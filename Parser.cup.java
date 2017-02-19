15fbfd745ab2cb3b552cd290791cfc1311cd54d0
fossildog117 <quantumbiology@hotmail.co.uk>
2017-02-19 13:37:22 +0000

    Update Parser.cup.java

--- Parser.cup.java
+++ Parser.cup.java
@@ -3,41 +3,40 @@ import java.util.ArrayList;
 
 parser code {:
 
-public boolean syntaxErrors = false;
+    public boolean syntaxErrors = false;
 
-private Lexer lexer;
+    private Lexer lexer;
 
-public Parser(Lexer input) {
-super(input);
-lexer = input;
+    public Parser(Lexer input) {
+        super(input);
+        lexer = input;
+    }
 
-}
-
-public void syntax_errors(Symbol symbol) {
-report_error("Syntax error at line " + (symbol.left+1) + " , column " + symbol.right);
-syntaxErrors = true;
-}
+    public void syntax_errors(Symbol symbol) {
+        report_error("Syntax error at line " + (symbol.left+1) + " , column " + symbol.right);
+        syntaxErrors = true;
+    }
 
 
 :};
 
 scan with {:
-return lexer.next_token();
+    return lexer.next_token();
 :};
 
 // ------- Grammar Values ------- START -------
 
 // Reserved words
-terminal MAIN, BOOLEAN, ICHARACTER, DICTIONARY, XRATIONAL, XINTEGER, IFLOAT, TOP, SEQ, IN, TRUE, FALSE, TYPEDEF, FUNCDEF, VOID, ALIAS, IF, ELIF, ELSE, FORALL, WHILE, READ, PRINT, RETURN, OD, DO, FI, LEN, THEN
+terminal MAIN, BOOLEAN, ICHARACTER, DICTIONARY, XRATIONAL, XINTEGER, IFLOAT, TOP, SEQ, IN, TRUE, FALSE, TYPEDEF, FUNCDEF, VOID, ALIAS, IF, ELIF, ELSE, FORALL, WHILE, READ, PRINT, RETURN, OD, DO, FI, LEN, THEN;
 
 // Brackets, operators, etc.
-terminal LESSEQUAL, MOREEQUAL, ISEQUAL, NOTEQUAL, LCURLY, RCURLY, LSQUARE, RSQUARE, EQUAL, SEMICOL, PLUS, MINUS, MULT, DIV, LPAREN, RPAREN, LESSTHAN, MORETHAN, NOT, AND, OR, POWER, DCOLON, COLON, COMMA, DOT
+terminal LESSEQUAL, MOREEQUAL, ISEQUAL, NOTEQUAL, LCURLY, RCURLY, LSQUARE, RSQUARE, EQUAL, SEMICOL, PLUS, MINUS, MULT, DIV, LPAREN, RPAREN, LESSTHAN, MORETHAN, NOT, AND, OR, POWER, DCOLON, COLON, COMMA, DOT;
 
 // Declarations
-terminal RATIONAL, FLOAT, INTEGER, NUMBER, CHARACTER, IDENTIFIER
+terminal RATIONAL, FLOAT, INTEGER, NUMBER, CHARACTER, IDENTIFIER;
 
 // Error flag
-terminal ERRORFLAG
+terminal ERRORFLAG;
 
 // ------- Grammar Values ------- END -------
 
